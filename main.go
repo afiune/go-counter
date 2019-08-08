c4cb94690bd49cba28c5896f0a45b31c5425641c
Salim Afiune <afiune@chef.io>
2019-08-08 12:41:00 +0200

    Basic HTTP counter written in Go
    Signed-off-by: Salim Afiune <afiune@chef.io>

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var REQUESTS int
+
+func main() {
+	url := ":1234"
+	fmt.Printf("Starting app listening on '%s'\n", url)
+	http.HandleFunc("/", processRequest)
+	if err := http.ListenAndServe(url, nil); err != nil {
+		panic(err)
+	}
+}
+
+func processRequest(w http.ResponseWriter, r *http.Request) {
+	// TODO(afiune) make it atomic
+	REQUESTS++
+	msg := fmt.Sprintf("{\"req_num\":%d,\"status\":\"processed\"}", REQUESTS)
+	fmt.Println(msg)
+	fmt.Fprintf(w, msg)
+}
